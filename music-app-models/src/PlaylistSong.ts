f48781a687276422cf2194dca0ba4a9b43f83167
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-08 03:23:00 -0300

    Correção de classes para respeitar as chaves dos relacionamentos no modelo de dados

--- music-app-models/src/PlaylistSong.ts
+++ music-app-models/src/PlaylistSong.ts
@@ -3,5 +3,7 @@ import { Constructable } from "./Constructable";
 export class PlaylistSong extends Constructable{
     playlist_owner_email: string;
     playlist_number: number;
-    song_id: number;
+    song_album_artist_id: string;
+    song_album_number: number;
+    song_number: number;
 }
