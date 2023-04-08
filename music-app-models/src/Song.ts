9909d704322b9c5e3e40d21b6b7fc998a2d53495
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-08 08:31:21 -0300

    Correção dos nomes de campos da entidade Song

--- music-app-models/src/Song.ts
+++ music-app-models/src/Song.ts
@@ -1,8 +1,8 @@
 import { Constructable } from "./Constructable";
 
 export class Song extends Constructable{
-    artist_id: string;
-    playlist_number: number;
+    album_artist_id: string;
+    album_number: number;
     song_number: number;
     name: string;
     length: number;
