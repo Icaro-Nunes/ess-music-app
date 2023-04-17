ac354742bdf9b3e063dbd17b231be7fd35780e25
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-17 01:20:08 -0300

    Corrigidos cenários e criados stepdefinitions para adicionar música

--- test-acceptance/stepdefinitions/song.ts
+++ test-acceptance/stepdefinitions/song.ts
@@ -7,20 +7,39 @@ import request = require("request-promise");
 const base_url = "http://localhost:3000/";
 const base_front_url = "http://localhost:4200";
 
-async function loginAsUser(user_id: string, password: string){
-    //Navegar até página de login
-    await browser.get(base_front_url);
-    await element(by.buttonText('Login')).click();
-    await expect(browser.getCurrentUrl()).to.eventually.equal(base_front_url + '/login');
-
-    //Realizar login
-    await $("input[formControlName='username']").sendKeys(<string> user_id);
-    await $("input[formControlName='password']").sendKeys(<string> password);
-    await element(by.buttonText('Login')).click();
-    await browser.wait(ExpectedConditions.urlIs(base_front_url + "/initial-page") , 30000);
-    await expect(browser.getCurrentUrl()).to.eventually.equal(base_front_url + "/initial-page");
-}
-
 defineSupportCode(function ({ Given, When, Then }){
-    
+    Given(/^Estou na página do álbum "([^\"]*)" do artista "([^\"]*)"$/, {timeout: 30000}, async (album: string, artist: string) => {
+        await element(by.cssContainingText('a', 'Visualizar Artistas')).click();
+
+        await expect(browser.getCurrentUrl()).to.eventually.equal(base_front_url + "/visualizar-artistas-admin");
+
+        await element(by.cssContainingText('td p', artist))
+            .element(by.xpath('..'))
+            .element(by.xpath('..'))
+            .element(by.buttonText('Visualizar Artista'))
+            .click();
+
+        await expect(browser.getCurrentUrl()).to.eventually.include(base_front_url + "/visualizar-artistas-admin/");
+
+        await browser.executeScript('window.scrollTo(0,document.body.scrollHeight);');
+
+        const elm = $("#tabelaAlbum")
+            .element(by.cssContainingText('td', album));
+
+        await elm.click();
+
+        await expect(browser.getCurrentUrl()).to.eventually.include(base_front_url + "/albumAdmin/");
+    })
+
+    When(/^Clico em "Cadastrar Musicas"$/, async () => {
+        await element(by.buttonText("Cadastrar Musicas")).click();
+
+        await expect(browser.getCurrentUrl()).to.eventually.include(base_front_url + "/albumAdmin/");
+        await expect(browser.getCurrentUrl()).to.eventually.include("/cadastrar-musica");
+    })
+
+    When(/^Preencho o campo de "Nome da Música" com "([^\"]*)"$/, async (name) => {
+        await $("#musica-titulo").clear();
+        await $("#musica-titulo").sendKeys(<string> name);
+    })
 })
